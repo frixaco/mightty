4222019220c561e9b1d529e502b26b900ca90685
frixaco <rustam21ashurmatov@gmail.com>
2026-03-22 23:34:34 +0500

    initial gpui and gpui-components setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use gpui::{
+    div, prelude::*, px, size, App, Application, Bounds, Context, Render, Window, WindowBounds,
+    WindowOptions,
+};
+use gpui_component::{button::*, Root, StyledExt};
+
+struct HelloWorld;
+
+impl Render for HelloWorld {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .v_flex()
+            .gap_2()
+            .size_full()
+            .items_center()
+            .justify_center()
+            .child("Hello, World!")
+            .child(
+                Button::new("ok")
+                    .primary()
+                    .label("Let's Go!")
+                    .on_click(|_, _, _| println!("Clicked!")),
+            )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Application::new().run(|cx: &mut App| {
+        // Initialize gpui-component
+        gpui_component::init(cx);
+
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |window, cx| {
+                let view = cx.new(|_| HelloWorld);
+                cx.new(|cx| Root::new(view, window, cx))
+            },
+        )
+        .unwrap();
+        cx.activate(true);
+    });
 }
